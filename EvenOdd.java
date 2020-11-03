fea16f87e76591522da7b1bf933a0e4dc1fcfc41
SUBASH-1998 <73734396+SUBASH-1998@users.noreply.github.com>
2020-11-03 10:58:00 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,12 @@
+class EvenOdd{
+ 	public static void main(String args[]){
+	 int a[]={2,5,8,11,14,17,22,24};
+	  for(int i=0;i<8;i++){
+		if(a[i]%2==0){
+			System.out.println(a[i]+"is a even number");
+		}else{
+			System.out.println(a[i]+"is a odd number");
+		}
+	}
+}
+}
