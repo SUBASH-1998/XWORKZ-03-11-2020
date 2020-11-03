11e9f0e7c5c401cab8ec2ee2cc26a8b75658dcd7
SUBASH-1998 <73734396+SUBASH-1998@users.noreply.github.com>
2020-11-03 10:52:58 +0530

    Add files via upload

---
+++ Divisibility.java
@@ -0,0 +1,10 @@
+class Divisibility{
+	public static void main(String args[]){
+	int a[]={12,33,25,24,38,23,48,93};
+	for(int i=0;i<8;i++){
+		if(a[i]%2==0){
+			System.out.println(a[i]);
+		}
+	}
+}
+}
